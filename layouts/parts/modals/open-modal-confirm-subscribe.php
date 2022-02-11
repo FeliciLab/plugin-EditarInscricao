a64023f6741d8fb96ef5f1d78cfa36384a3e69e1
Junior-Shyko <franciscoanto@gmail.com>
2022-02-11 13:17:24 -0300

    🚀 - Adicionando codigo para utilização do modulo do Angular

--- layouts/parts/modals/open-modal-confirm-subscribe.php
+++ layouts/parts/modals/open-modal-confirm-subscribe.php
@@ -1,26 +1,33 @@
-<div class="remodal modal-border" ng-controller="OpportunityController" data-remodal-id="modal-info-registration-confirm">
-        <button data-remodal-action="close" class="remodal-close"></button>
-    <h3 class="remodal-title">
-    <?php print_r($infoModal['titleModal']); ?>
-    </h3>
-    <div>
-        <h4 style="color: #F26822; font-weight: bold;">
-        Você conferiu os seus dados? 
-        </h4>
-    </div>
-    <div>
-        <p>Ao confirmar, <b>você poderá editá-la somente</b> durante o período de inscrições.
-        </p>
-        <p>
-            Para editar sua inscrição, acesse o menu <br>
-            <b>Meu Perfil > Minhas Inscrições. </b>
-        </p>
-    </div>
-    <br>
-    <div style="float: right;">
-    <button data-remodal-action="cancel" class="btn btn-default" title="Sair da resposta" style="margin-right: 15px;"> Voltar</button>
-    
-    <a class="btn btn-success btn-registration" ng-click="sendRegistration()" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Confirmar"); ?></a>
-    </form>
+<?php
+    $this->bodyProperties['ng-app'] = "entity.app";
+    $this->bodyProperties['ng-controller'] = "EntityController";
+    $this->jsObject['angularAppDependencies'][] = 'errorValidation';
+?>
+<div ng-app="errorValidation">
+    <div class="remodal modal-border" ng-controller="ValidationController" data-remodal-id="modal-info-registration-confirm">
+            <button data-remodal-action="close" class="remodal-close"></button>
+        <h3 class="remodal-title">
+        <?php print_r($infoModal['titleModal']); ?>
+        </h3>
+        <div>
+            <h4 style="color: #F26822; font-weight: bold;">
+            Você conferiu os seus dados? 
+            </h4>
+        </div>
+        <div>
+            <p>Ao confirmar, <b>você poderá editá-la somente</b> durante o período de inscrições.
+            </p>
+            <p>
+                Para editar sua inscrição, acesse o menu <br>
+                <b>Meu Perfil > Minhas Inscrições. </b>
+            </p>
+        </div>
+        <br>
+        <div style="float: right;">
+        <button data-remodal-action="cancel" class="btn btn-default" title="Sair da resposta" style="margin-right: 15px;"> Voltar</button>
+        
+        <a class="btn btn-success btn-registration" ng-click="sendRegistration()" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Confirmar"); ?></a>
+        </form>
+        </div>
     </div>
 </div>
